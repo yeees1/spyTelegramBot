c48789e8a20c154e8e733f879dfb7dcf76be56a4
yes? <you@example.com>
2026-01-24 22:43:36 +0300

    fix

--- database.py
+++ database.py
@@ -235,7 +235,7 @@ class DB:
         )
     async def insertNewUser(self, chat_id, username):
         await self.execute(
-            "INSERT OR IGNORE INTO allusers (chat_id, username) VALUES (%s, %s)",
+            "INSERT IGNORE INTO allusers (chat_id, username) VALUES (%s, %s)",
             (chat_id, username)
         )
 
